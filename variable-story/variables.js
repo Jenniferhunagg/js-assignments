70d50d7bada21c052a0e4fd5edd80c55cec84e56
Jenniferhunagg <111556953+Jenniferhunagg@users.noreply.github.com>
2023-01-11 11:03:19 +0800

    1/11/2023

---
+++ variable-story/variables.js
@@ -0,0 +1,13 @@
+const name ="Jennifer"
+const gender= "female"
+const school= "Pacific American School"
+
+const age = prompt("How old am I?")
+const height = prompt("How tell am I?")
+const friend = prompt("What is my best freind in Web Development class?")
+
+document.write("<div class='top'>Hi. Nice to meet you !! </div>")
+document.write("<div class='name'> My name is " + name + "</div>")
+document.write("<p class='information1'> I am "+ age + " years old. My height is "+ height + " cm. I am study at " + school +". I like to play or watch sport. For example, I like to watch NBA and soccer game. My favorite sport is basketball.</p>")
+document.write("<p class='information2'> After I lost " + friend+ ", I feel my design grade will be lower. Also, I feel lonely in class.</p>")
+document.write(`<img class="image" src="312492609_1046640926733188_9205700947956550255_n.jpg"></img>`)
