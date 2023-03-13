58609ee2dc9c5b97d21add721c6122cc9b1be6df
Jenniferhunagg <111556953+Jenniferhunagg@users.noreply.github.com>
2023-03-13 09:46:40 +0800

    typing

--- typing/typing.js
+++ typing/typing.js
@@ -1,4 +1,29 @@
-const typing =[[82,25],[76,21],[85,26],[89,32],[85,29],[83,29],[88,33],[92,40],[91,38],[90,34],[91,35]]
+const typing =[
+    [82,76,85,89,85,83,88,92,91,90,91,86 ],
+    [25,21,26,32,29,29,33,40,38,34,35,36 ],
+]
+let sumAcc= 0
+let sumWPM=0
+typing[0].forEach(function(element) {
+    sumAcc = sumAcc + element
+})
+let avgAcc = sumAcc / typing[0].length
+document.querySelector(".scoreAccAv").innerText = avgAcc
 
+typing[1].forEach(function(element) {
+    sumWPM = sumWPM + element
+})
+let avgWPM = sumWPM / typing[0].length
+document.querySelector(".scoreWPMAv").innerText = avgWPM
 
-document.querySelector(".scoreAccAv").innerText = numQuarter
+let lastAcc = typing[0][typing[0].length - 1];
+document.querySelector(".latesAcc").innerText = "Accuracy:" + lastAcc
+
+let lastWPA = typing[1][typing[1].length - 1];
+document.querySelector(".latestWAP").innerText = "WPM:" +lastWPA
+
+const biggest_numberWPM = Math.max(...typing[1]);
+document.querySelector(".scoreWPM").innerText = biggest_numberWPM
+
+const biggest_numberAcc = Math.max(...typing[0]);
+document.querySelector(".scoreAcc").innerText = biggest_numberAcc
